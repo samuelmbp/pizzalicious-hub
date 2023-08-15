7a20f565a866445c0cfbd16bab2462375468a046
Samuel Raducan <samuel.thembp@gmail.com>
2023-08-15 11:46:05 +0200

    Built the app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Pizzalicious Hub Co.</Link>
+
+      <p>Username</p>
+    </header>
+  );
+};
+
+export default Header;
