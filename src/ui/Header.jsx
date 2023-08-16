6cdc1ed7e5523f93c65dc52ec39ec1610347c2d8
Samuel Raducan <samuel.thembp@gmail.com>
2023-08-16 10:43:09 +0200

    Fetch orders

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">Pizzalicious Hub Co.</Link>
 
-      <p>Username</p>
+      <SearchOrder />
+      <p>Samuel</p>
     </header>
   );
 };
