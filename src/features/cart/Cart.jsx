bbae1216728b73fca02ff1d9f65a0e4ee9d38103
Samuel Raducan <samuel.thembp@gmail.com>
2023-08-18 10:08:17 +0200

    Style pizza menu

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -36,8 +36,9 @@ function Cart() {
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Button to="/order/new">Order pizzas</Button>
-        {/* <Link to="/order/new">Order pizzas</Link> */}
+        <Button type="primary" to="/order/new">
+          Order pizzas
+        </Button>
         <button>Clear cart</button>
       </div>
     </div>
