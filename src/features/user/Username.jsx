1176a4fda32c1d1f898670a2a54fce150d5ed931
Samuel Raducan <samuel.thembp@gmail.com>
2023-08-21 09:38:18 +0200

    Model user state with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Username = () => {
-  return <div className="hidden text-sm font-semibold md:block">Samuel</div>;
+  const username = useSelector((state) => state.user.username);
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 };
 
 export default Username;
